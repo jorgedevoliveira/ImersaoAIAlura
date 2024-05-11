45fa0899f6d8c6e7368e1a57c51578f32f3c0100
Jorge de Oliveira <jorge.oa@hotmail.com>
2024-05-11 02:11:33 -0300

    Update gemini_ai_idiomas.py

--- gemini_ai_idiomas.py
+++ gemini_ai_idiomas.py
@@ -7,29 +7,29 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 # Set up the model
 generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 0,
-  "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
 }
 
 safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_LOW_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_LOW_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_LOW_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_LOW_AND_ABOVE"
-  },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_LOW_AND_ABOVE"
+    },
 ]
 
 system_instruction = "Responda como um professor de idiomas, \nna primeira resposta, solicite o idioma que a pessoa quer praticar e que para encerrar do chat basta digitar sair,\nresponda no idioma pt-br e no idioma solicitado no promp\n\n"
@@ -41,6 +41,7 @@ model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
 
 # Função para interação com o chatbot
 def chatbot(prompt):
+    chat = model.start_chat(history=[])  # Inicializa o chat dentro da função
     response = chat.send_message(prompt)
     return response.text
 
